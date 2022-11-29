16baf54c3f61cf446ca03ffb87df44576fe88454
Joe King <j.king92@gmail.com>
2022-11-29 16:52:32 -0700

    adds the songs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/songs', to: 'songs#index'
+  # can receive request for /songs and route to controller songs index
+  # name of controller and action of controller
 end
