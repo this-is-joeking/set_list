84a897e9561b072a016d8de7f8391e5e9909d252
Joe King <j.king92@gmail.com>
2022-11-30 22:37:00 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+prince = Artist.create!(name: 'Prince')
+purple = prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+rasp = prince.songs.create!(title: 'Raspberry Beret', length: 245, play_count: 103874)
